1bca2b10c2169d4a0d22dd8482a692f844acbe58
Xin Liang <XLiang@suse.com>
2026-09-02 15:34:39 +0800

    fix(ui_resource): validate operation and interval in resource trace
    - Reject <op> that is not a known RA operation with a clear error
    - Recognize interval values with time units (e.g. "10s", "1m") using
      utils.crm_msec instead of str.isnumeric()
    - Add unit tests for both cases in test_ratrace.py

--- crmsh/ui_resource.py
+++ crmsh/ui_resource.py
@@ -720,7 +720,7 @@ class RscMgmt(command.UI):
                 if trace_dir is not None:
                     context.fatal_error(usage)
                 trace_dir = arg
-            elif arg.isnumeric():
+            elif utils.crm_msec(arg) != -1:
                 if interval is not None:
                     context.fatal_error(usage)
                 interval = arg
@@ -733,6 +733,8 @@ class RscMgmt(command.UI):
         if op == "probe":
             op = "monitor"
             interval = interval or "0"
+        if op is not None and op not in constants.op_cli_names:
+            context.fatal_error("Unknown operation: %s" % (op))
         if op is None:
             self._trace_resource(context, rsc_id, rsc, trace_dir)
         elif interval is None:
@@ -748,10 +750,6 @@ class RscMgmt(command.UI):
         node_str = " on node %s" % (", ".join(running_nodes)) if running_nodes else ""
         logger.info("Trace for %s%s is written to %s/%s%s",
                     rsc_id, ":" + op if op else "", trace_dir, rsc_type, node_str)
-        if op and op != "monitor":
-            logger.info("Trace set, restart %s to trace the %s operation", rsc_id, op)
-        else:
-            logger.info("Trace set, restart %s to trace non-monitor operations", rsc_id)
         return True
 
     def _remove_trace(self, rsc, op_node):
